6e94b655f9b508e6546de6d729c066aae2619632
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-20 21:52:13 +0300

    Fix: previous revert shouldnt happen

---
+++ MovieExplorer/Models/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieExplorer.Models.ViewModels
+{
+    public class MovieListViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? PosterPath {  get; set; } 
+        public string? ReleaseDate { get; set; }
+    }
+}

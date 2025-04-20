29a0dc6687391cb48beee8ab250b27e3dfc86386
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-20 18:55:35 +0300

    Add project files.

---
+++ MovieExplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieExplorer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

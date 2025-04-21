28d71bde1471dcef182cf390fb4f9890bd55e233
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-21 18:56:56 +0300

    implemented views and related things

--- MovieExplorer/Models/ViewModels/MovieListViewModel.cs
+++ MovieExplorer/Models/ViewModels/MovieListViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class MovieListViewModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Title { get; set; }
         public string? PosterPath {  get; set; } 
         public string? ReleaseDate { get; set; }

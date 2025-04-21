b24cfaf1cac6a644c2ab534944418e0cb6bc2675
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-21 18:56:27 +0300

    implemented the controller

---
+++ MovieExplorer/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieExplorer.Services;
+
+namespace MovieExplorer.Controllers
+{
+    public class MoviesController(IMovieService movieService):Controller
+    {
+        public async Task<IActionResult> Latest()
+        {
+            var latestMovies=await movieService.GetLatestMovies();
+            return View(latestMovies);
+        }
+
+        public async Task<IActionResult> TopRated()
+        {
+            var topMovies= await movieService.GetTopRatedMovies();
+            return View(topMovies);
+        }
+    }
+}

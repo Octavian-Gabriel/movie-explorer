e7027d766020da7916b32aab24e95114cc453b68
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-23 09:38:29 +0300

    Added Users in dbContext

--- MovieExplorer/Data/MovieExplorerDbContext.cs
+++ MovieExplorer/Data/MovieExplorerDbContext.cs
@@ -6,5 +6,6 @@ namespace MovieExplorer.Data
     public class MovieExplorerDbContext(DbContextOptions<MovieExplorerDbContext> options) : DbContext(options)
     {
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }

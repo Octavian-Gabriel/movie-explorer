a21fcfb9afdd9a96483643ce5872a29d99a440e1
Octavian-Gabriel <cotfasa.octavian.gabriel@gmail.com>
2025-04-23 07:45:55 +0300

    linked Comment model to an User model

--- MovieExplorer/Models/Comment.cs
+++ MovieExplorer/Models/Comment.cs
@@ -2,11 +2,11 @@
 {
     public class Comment
     {
-       public int Id { get; set; }
-
+        public int Id { get; set; }
         public int MovieId {  get; set; }
         public string UserId { get; set; } = string.Empty;
         public string UserName { get; set;} = string.Empty;
+        public required User User { get; set; }
         public string Content {  get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }
